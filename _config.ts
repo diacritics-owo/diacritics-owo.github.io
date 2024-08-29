52372c1227ffee81dfc01e3060eeceabf6f7cfac
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-08-29 07:52:02 +0530

    layout

--- _config.ts
+++ _config.ts
@@ -1,5 +1,26 @@
 import lume from 'lume/mod.ts';
+import sass from 'lume/plugins/sass.ts';
+import inline from 'lume/plugins/inline.ts';
+import minifyHTML from 'lume/plugins/minify_html.ts';
+import tailwindcss from 'lume/plugins/tailwindcss.ts';
+import postcss from 'lume/plugins/postcss.ts';
+import remark from 'lume/plugins/remark.ts';
+
+import typography from 'npm:@tailwindcss/typography';
 
 const site = lume();
+
+site.use(remark());
+site.use(sass());
+site.use(
+  tailwindcss({
+    options: {
+      plugins: [typography],
+    },
+  })
+);
+site.use(postcss());
+site.use(inline());
+site.use(minifyHTML());
 
 export default site;
--- index.md
+++ index.md
@@ -2,4 +2,19 @@
 layout: layouts/main.vto
 ---
 
-# Hello
+# Hello!
+
+I'm diacritics-owo \[she/they], a primarily self-taught developer. I've been programming for several years, mostly in Rust, TypeScript, and Java.
+
+## Interests
+
+- Mathematics
+- Programming language theory and design
+- Reverse engineering
+- Minecraft modding
+
+## Places
+
+[GitHub](https://github.com/diacritics-owo)
+
+[Modrinth](https://modrinth.com/user/diacritics-owo)
